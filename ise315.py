81ec33fec11de0fdbe1941af9148a928315e804e
yrqbs <qabosyasser@gmail.com>
2026-07-01 20:34:56 +0300

    Update ise315.py

--- ise315.py
+++ ise315.py
@@ -330,11 +330,11 @@ elif menu == "Grade Calculator 🧮":
     with col1:
         st.subheader("Enter Your Scores (%)")
         with st.container(border=True):
-            m1_p = st.number_input("First Exam (%)", 0.0, 100.0, 78.5)
-            m2_p = st.number_input("Second Exam (%)", 0.0, 100.0, 81.0)
-            final_p = st.number_input("Final Exam (%)", 0.0, 100.0, 88.0)
-            hw_p = st.number_input("HW Raw Score (%)", 0.0, 100.0, 99.3)
-            att_p = st.number_input("Attendance Raw Score (%)", 0.0, 100.0, 67.2)
+            m1_p = st.number_input("First Exam (%)", 0.0, 100.0, )
+            m2_p = st.number_input("Second Exam (%)", 0.0, 100.0, 0)
+            final_p = st.number_input("Final Exam (%)", 0.0, 100.0, 0)
+            hw_p = st.number_input("HW Raw Score (%)", 0.0, 100.0, 0)
+            att_p = st.number_input("Attendance Raw Score (%)", 0.0, 100.0, )
             curve = st.slider("Normalization Impact (%)", -10, 10, 0)
     with col2:
         st.subheader("📊 Your Performance Dashboard")
